54a97b9160ffa4282b47b69f7056fd14db31da8c
OliverL <oliver.litavszky>
2020-09-15 09:30:15 +0200

    loop

--- prog.js
+++ prog.js
@@ -1,10 +1,18 @@
 let names = ["Nickas", "Victor", "Parvin"]
-let i = 0
 let text = ""
+let i = 0
+let rev = names.reverse()
 
-while (names[i]){
-    text += names[i]
-    i++
+
+for (let x=0; x<=100; x++) {
+    if (x % 2 === 0) {
+            console.log(x);   
     }
+}
 
-console.log(i)
+for (let x=5; x<=79; x++) {
+    if (x % 3 === 0) {
+            console.log(x);   
+    }
+}
+console.log(rev)
