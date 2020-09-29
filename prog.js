9f169c72f52e14ed87fd505c8e8ac23315f49080
OliverL <oliver.litavszky>
2020-09-29 09:03:50 +0200

    filly

--- prog.js
+++ prog.js
@@ -1,18 +1,49 @@
-let names = ["Nickas", "Victor", "Parvin"]
-let text = ""
-let i = 0
-let rev = names.reverse()
+const readline = require('readline')
 
+const r1 = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
-for (let x=0; x<=100; x++) {
-    if (x % 2 === 0) {
-            console.log(x);   
-    }
+let number = r1.question("Write a number: ", (answer) => {
+    let numbers = answer.split(' ')
+    console.log(numbers)
+
+    let num1 = numbers[0].split('').reverse()
+    let num2 = numbers[1].split('').reverse()
+
+    console.log(num1)
+
+if (!answer){
+    console.log("You need to write something u dumbo!")
+    return;
+}
+else if(answer < 1 || answer > 100) {
+    console.log("The number must be smaller or equal to 100 and greater or equal to 1")
+    return;
 }
 
-for (let x=5; x<=79; x++) {
-    if (x % 3 === 0) {
-            console.log(x);   
+for (let i = 1; i <= answer; i++) {
+    console.log(i + " - That's it!")
     }
-}
-console.log(rev)
+
+//piece of cake
+let s = 10
+let h = 4
+let v = 7
+
+let A = h*v
+let B = (s-h) * v
+let C = h * (s-v)
+let D = (s-h) * (s-v)
+
+let pieces = [A,B,C,D]
+
+pieces.sort()
+
+let largestPiece = pieces[pieces.length -1]
+console.log(largestPiece)
+
+
+})
+
