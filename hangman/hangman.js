f16738c4754d50706307582a9d318f4509686028
OliverL <oliver.litavszky>
2020-12-10 14:20:31 +0100

    ayy

--- hangman/hangman.js
+++ hangman/hangman.js
@@ -1,55 +1,177 @@
-let Wordlist = [
-"scrape",
-"elbow",
-"printer",
-"peanut",
-"laborer",
-"digress",
-"crowd",
-"noble",
-"banner",
-"convenience",
-"check",
-"cheek",
-"city",
-"digital",
-"character",
-"complication",
-"spell",
-"great",
-"familiar",
-"spine",
-"stake",
-"systematic",
-"guess",
-"pattern",
-"quaint",
-"criminal",
-"arrogant",
-"prisoner",
-"prevent",
-"philosophy",
-"compartment",
-"offend",
-"tongue",
-"wire",
-"folklore",
-"finance",
-"headquarters",
-"correction",
-"current",
-"tense",
-"dividend",
-"draft",
-"disguise",
-"common",
-"preparation",
-"sea",
-"daughter",
-"manager",
-"number",
-"experienced",
-]
-let word = Wordlist[Math.floor(Math.random() *Wordlist.length)];
+// Variablar
+let word = '';
+let dArray = [];
+let fails = 0;
 
-console.log(word);
+// Array av 75 ord
+const wordList = {
+  easy: [
+    'fold',
+    'eye',
+    'slam',
+    'far',
+    'fist',
+    'chip',
+    'rest',
+    'brag',
+    'show',
+    'sex',
+    'mind',
+    'name',
+    'orgy',
+    'zone',
+    'pie',
+    'film',
+    'full',
+    'lump',
+    'halt',
+    'bin',
+    'bend',
+    'my',
+    'gown',
+    'old',
+    'hand'
+  ],
+  normal: [
+    'stroke',
+    'relieve',
+    'reward',
+    'rehearsal',
+    'fine',
+    'insight',
+    'false',
+    'trip',
+    'quarrel',
+    'extreme',
+    'archive',
+    'dividend',
+    'monkey',
+    'nursery',
+    'review',
+    'expose',
+    'beef',
+    'cream',
+    'glide',
+    'purpose',
+    'counter',
+    'policy',
+    'statement',
+    'strange',
+    'gradual',
+  ],
+  hard: [
+    "qualification",
+    "extraterrestrial",
+    "consideration",
+    "inappropriate",
+    "enthusiasm",
+    "constituency",
+    "possibility",
+    "ideology",
+    "association",
+    "environmental",
+    "operational",
+    "superintendent",
+    "examination",
+    "hospitality",
+    "revolutionary",
+    "beneficiary",
+    "responsibility",
+    "sensitivity",
+    "investigation",
+    "representative",
+    "multimedia",
+    "intermediate",
+    "administration",
+    "discrimination",
+    "vegetarian",
+  ]
+}
+
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.on('line', (line) => {
+  wordCheck(line);
+})
+
+// Vad svårighets graden är
+const difficulties = ["easy", "normal", "hard"];
+  const choose = difficulties[random(3)];
+  word = wordList[choose][random(random(wordList[choose].length))];
+  dArray = Array(word.length);
+  fails = 0;
+
+  // Funktion till random orden
+  function random(max) {
+    return Math.floor(Math.random() * (max));
+  }
+
+function difficulty() {
+  console.log('You have 3 difficulties to chose from; "easy", "normal" or "hard".' );
+
+  console.log('Difficulty is ' + choose + '\n' + dArray.join ('_ ') + '_');
+  rl.prompt();  
+}
+
+// Om orden finns
+function wordCheck(letter) {
+  let failed;
+  for (let i = 0; i < word.length; i++) {
+    if (word[i].toLowerCase() === letter) {
+      dArray[i] = word[i];
+      sap = true;
+    } else if (dArray[i] === undefined) {
+      dArray[i] = '_';
+    }
+  }
+
+  failed ? failed = false : fails++;
+  
+
+  // Om man har förlorat
+  if (fails === 6) {
+    console.log(fails);
+    console.log('Game over, you are hanged',
+    
+    ` 
+    +===++
+    |   ||              mmmmmmmmmmmmmmmmmmmmmmmmmmmmmm
+   😵   ||            I, QUEEN MARGARETH, AM VICTORIUS!
+   /T\\  ||       👸/    wwwwwwwwwwwwwwwwwwwwwwwwwwwwww
+   / \\  ||       /T
+        ||       / \\
+  ======================` 
+
+    );
+    console.log('Word is ', word);
+    difficulty();
+    return;
+  } else {
+    console.log(fails);
+  }
+  // Om man har vunnit
+  dArray.includes('_') ? rl.prompt() : console.log('You won! I am surprised!',
+  ` 
+    +===++
+    |   ||              
+        ||            
+   \\😁/ ||           
+     T  ||       
+    / \\ ||       
+  ==========` );
+  console.log(dArray.join(' '));
+}
+
+// Om man lämnar utan att spela
+rl.on('close', function()
+{
+  console.log('Goodbye! ... <|> ... <|> ... <|> ... loser :( ')
+  process.exit(1);
+});
+
+difficulty();
---
+++ hangman/planering.md
@@ -0,0 +1,13 @@
+### Vad ska jag göra och hur ser spelet ut? ###
+*1* Jag ska göra en hängagubbe spel som använder 50st random ord.
+*2* Spelet ska låta användaren välja om hända gubben är en av 4 races, antinger "Human", "Dwarf, "Elf" och "Orc".
+*3* Spelet funkar precis som ett vanligt hängagubbe spel, dvs "bygga" klart gubben för att hänga humanoiden. Användaren har 6 försök på sig för att gissa ordet spelet har valt.
+*4* Om användaren har förlorat så måste hen börja om, om användaren har vunnit så får hen en belöning.
+--=-|---------------------------------------------------------------------------------------------------------------------------------------|=--
+### Hur ska jag göra detta? ###
+*1* Jag ska samla ihop ord från nätet som är randomiserat, detta blir på engelska dock.
+*2* Jag ska göra en array av 50 ord och senare slumpa orden.
+*3* Jag ska använda mig av readline.
+*4* Jag ska 
+*5*
+*6*
